d59080d9e1d326ea907d1f8511fd911180fa142d
Juan Campiño <jcampinotoro@gmail.com>
2023-02-02 11:03:44 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using iTextSharp.text.pdf;
+
+namespace PoC_Pdf {
+    internal class Program {
+
+        public static void Main(string[] args) {
+            string inputFile = @"D:\Escritorio\Work\test.pdf";
+            string outputFile = @"D:\Escritorio\Work\testOut.pdf";
+            string password = "rE3@xno6pfTK8gKjHy&@M@9m7CjyjT@";
+
+            using(Stream input = new FileStream(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read)) {
+                using(Stream output = new FileStream(outputFile, FileMode.Create, FileAccess.Write, FileShare.None)) {
+                    var reader = new PdfReader(input);
+                    PdfEncryptor.Encrypt(reader, output, true, null, password, PdfWriter.ALLOW_SCREENREADERS);
+                }
+            }
+        }
+
+    }
+}
