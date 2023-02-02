88451ac284424ef761a145c50c95c386e2f76bc6
Juan Campiño <jcampinotoro@gmail.com>
2023-02-02 11:20:33 -0500

    sync

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@ namespace PoC_Pdf {
     internal class Program {
 
         public static void Main(string[] args) {
-            string inputFile = @"D:\Escritorio\Work\test.pdf";
-            string outputFile = @"D:\Escritorio\Work\testOut.pdf";
-            string password = "rE3@xno6pfTK8gKjHy&@M@9m7CjyjT@";
+            const string inputFile = @"D:\Escritorio\Work\test.pdf";
+            const string outputFile = @"D:\Escritorio\Work\testOut.pdf";
+            const string password = "rE3@xno6pfTK8gKjHy&@M@9m7CjyjT@";
 
             using(Stream input = new FileStream(inputFile, FileMode.Open, FileAccess.Read, FileShare.Read)) {
                 using(Stream output = new FileStream(outputFile, FileMode.Create, FileAccess.Write, FileShare.None)) {
                     var reader = new PdfReader(input);
-                    PdfEncryptor.Encrypt(reader, output, true, password, password, PdfWriter.ALLOW_PRINTING);
+                    PdfEncryptor.Encrypt(reader, output, true, null, password, PdfWriter.ALLOW_PRINTING);
                 }
             }
         }
